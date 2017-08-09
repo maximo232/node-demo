f8d66450c34c9d6fef0f2cc5e29de9ecaf10ab1c
maximo232 <mk.max@hotmail.com>
2017-08-09 14:53:48 +0100

    All Project

---
+++ public/javascripts/shout_javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    //set a listener on the textbox
+    $('#input').on("change", (evt) => {
+        let text = $('#input').val();
+        // the {text: text} sends a parameter named text with the
+        $.get('/shout', {text: text})
+            .done((data) => {
+                console.log(data);
+                $('#results').prepend('<li>' + data['result'] + '</li>');
+                $('#input').val(''); //reset the textbox
+            })
+            .fail((xhr) => {
+                alert('Problem contacting server');
+                console.log(xhr);
+            });
+        });
+    });
